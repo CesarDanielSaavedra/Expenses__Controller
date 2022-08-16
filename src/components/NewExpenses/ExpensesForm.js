f581cce0d2fa512e51cbc8fde0a8b13976990c87
Daniel Saavedra <cesardsaavedra@gmail.com>
2022-08-16 17:08:18 -0300

    A states are added to storaged the values of title, amount and date

--- src/components/NewExpenses/ExpensesForm.js
+++ src/components/NewExpenses/ExpensesForm.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpensesForm.css";
+
+
 
 const ExpensesForm = () => {
 
+    const [enteredTitle, setEnteredTitle] = ("");
+    const [enteredamount, setEnteredAmount] = ("");
+    const [enteredDate, setEnteredDate] = ("");
+
+
     const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        console.log(event.target.value);
+    
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
         console.log(event.target.value);
     }
 
@@ -16,11 +34,11 @@ const ExpensesForm = () => {
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label> 
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label> 
-                    <input type="date" />
+                    <input type="date" onChange={dateChangeHandler} />
                 </div>
             </div>
         </form>            
