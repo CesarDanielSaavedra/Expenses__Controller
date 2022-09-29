ac41da9dbd6c33960624c3e962b0d40dbb391045
Daniel Saavedra <cesardsaavedra@gmail.com>
2022-09-29 15:07:53 -0300

    Filter components added

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,37 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Expense.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expense = (props) => {
-  // const row = [];
-
-  // for (let index = 0; index < props.data.length; index++) {
-  //   row.push(
-  //     <ExpenseItem
-  //       title={props.data[index].title}
-  //       amount={props.data[index].amount}
-  //       date={props.data[index].date}
-  //     ></ExpenseItem>
-  //   );
-  // }
-
+ 
   const expenses = props.data;
 
   console.log(expenses);
 
-  return <Card className="expenses">{
-    expenses.map( (expense) => {
-      return (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          key={expense.id}
-        ></ExpenseItem>
-      );
-    })
-  }</Card>;
+  const [selectedFilterYear, setSelectedFilterYear] = useState("2021");
+
+  const changeYearFilterHandler = (year) => {
+
+    setSelectedFilterYear(year);
+
+  }
+
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedFilterYear;
+  });
+
+  return (<Card className="expenses">
+
+    <ExpensesFilter onChangeYearFilter={changeYearFilterHandler} yearSelected={selectedFilterYear}/>
+
+    {filteredExpenses.map( (expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            key={expense.id}
+          ></ExpenseItem>
+        );
+      })
+    }
+  </Card>
+
+  );
 }
 
 export default Expense;
