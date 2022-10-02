a7b9705dd13fd59316e5dfee911027e824c8c0bb
Daniel Saavedra <cesardsaavedra@gmail.com>
2022-10-02 20:20:28 -0300

    delet the boton 'change tittle' in ExpenseItem component

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -9,14 +9,10 @@ const Expense = (props) => {
  
   const expenses = props.data;
 
-  console.log(expenses);
-
   const [selectedFilterYear, setSelectedFilterYear] = useState("2021");
 
   const changeYearFilterHandler = (year) => {
-
     setSelectedFilterYear(year);
-
   }
 
   const filteredExpenses = expenses.filter((expense) => {
@@ -26,19 +22,6 @@ const Expense = (props) => {
   return (<Card className="expenses">
 
     <ExpensesFilter onChangeYearFilter={changeYearFilterHandler} yearSelected={selectedFilterYear}/>
-
-    {/*this is the funtionality (map method) to reproduce in a new comp*/}
-    {/* {filteredExpenses.map( (expense) => {
-        return (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            key={expense.id}
-          ></ExpenseItem>
-        );
-      })
-    } */}
     <ListItemFiltered expenses={filteredExpenses} />
   </Card>
 
--- src/components/Expenses/ListItemFiltered.js
+++ src/components/Expenses/ListItemFiltered.js
@@ -4,18 +4,22 @@ import ExpenseItem from "./ExpenseItem";
 const ListItemFiltered = (props) => {
 
   // props.filteredExpenses = filteredExpenses
+  if (props.expenses.length === 0) {
+    return ( 
+      <h2> No expenses found for this selected Year </h2>
+    );
+  }
 
   return (
     <ul>
-      {props.expenses.map( (expense) => {
-        return (<ExpenseItem
+      {props.expenses.map( (expense) => (
+        <ExpenseItem
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
           key={expense.id}
         ></ExpenseItem>
-        );
-        })}
+      ))}
     </ul>
   );
 }
